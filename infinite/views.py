b882e17cd0202359ba609d1e9bba257845670d8c
zhuowang1997 <2542316w@student.gla.ac.uk>
2021-08-03 22:23:23 +0800

    ZHUO

--- infinite/views.py
+++ infinite/views.py
@@ -31,13 +31,7 @@ def index(request):
 def about(request):
     # Construct a dictionary to pass to the template engine as its context.
     # Note the key boldmessage matches to {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
-    # Return a rendered response to send to the client.
-    # We make use of the shortcut function to make our lives easier.
-    # Note that the first parameter is the template we wish to use.
-    if request.session.test_cookie_worked():
-        print("TEST COOKIE WORKED!")
-        request.session.delete_test_cookie()
+    context_dict = {}
 
     visitor_cookie_handler(request)
     context_dict['visits'] = request.session['visits']
