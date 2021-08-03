092afa6fd545343374b7edd5ec688edaafc61442
zhuowang1997 <2542316w@student.gla.ac.uk>
2021-08-03 21:39:54 +0800

    ZHUO

--- infinite_team_project/settings.py
+++ infinite_team_project/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'infinite',
+    'registration',
 ]
 
 MIDDLEWARE = [
@@ -132,4 +133,12 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
 
-LOGIN_URL = 'infinite:login'
+# If True, users can register.
+REGISTRATION_OPEN = True
+# If True, the user will be automatically logged in after registering.
+REGISTRATION_AUTO_LOGIN = True
+# The URL that Django redirects users to after logging in.
+LOGIN_REDIRECT_URL = 'infinite:index'
+# The page users are directed to if they are not logged in.
+# This was set in a previous chapter. The registration package uses this, too.
+LOGIN_URL = 'auth_login'
